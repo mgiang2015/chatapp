e573d23e74a123596cbffee905aeb3f8799d1a54
Le Minh Giang <mgiang2015@gmail.com>
2023-09-21 21:40:38 +0800

    first comit

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Init app
+app = Flask(__name__)
+
+# To enable encryption
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+# Wrap app around SocketIO
+socketio = SocketIO(app)
+
+app.route('/')
+def hello():
+    return "hello!"
+
+def messageReceived(methods=['GET', 'POST']):
+    print("Message received!!")
+
+
+@socketio.on('my event')
+def handle_event(json, methods=['GET', 'POST']):
+    print('Received event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
